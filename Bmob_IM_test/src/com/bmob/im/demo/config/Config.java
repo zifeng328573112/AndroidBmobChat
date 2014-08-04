8491663855cc381ec0d258d2fffb856ebd5839b7
ZiFeng <zifeng842266060@163.com>
2014-08-04 16:40:21 +0800

    Android Bmob Chat

---
+++ Bmob_IM_test/src/com/bmob/im/demo/config/Config.java
@@ -0,0 +1,14 @@
+package com.bmob.im.demo.config;
+
+/** 系统变量
+  * @ClassName: Config
+  * @Description: TODO
+  * @author smile
+  * @date 2014-6-17 上午9:40:11
+  */
+public class Config {
+
+	/** 可以自己申请application Id*/
+	public static String applicationId = "c28f971e0d0b1d2a6d3d0a5505fd8112";
+}
+  
